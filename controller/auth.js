af37ea7f5c90e6280100623424371ceda6fc2a5f
jadjaram <97999728+jadjaram@users.noreply.github.com>
2022-11-11 09:36:21 -0500

    Update auth.js

--- controller/auth.js
+++ controller/auth.js
@@ -34,7 +34,7 @@ export const login = async(req, res, next) =>{
         
         const token = jwt.sign({id:user._id, isAdmin: user.isAdmin}, process.env.JWT)
 
-        const {password, isAdmin, ...otherDetail} = user._doc
+        const {password, role, ...otherDetail} = user._doc
 
         res
         .cookie('access_token', token,{
